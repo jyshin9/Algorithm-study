658f9b290eba6e6871ab2f1362d76b14eb685dab
jyshin9 <baekju092@gmail.com>
2023-07-11 11:14:47 +0900

    first commit

---
+++ while1_vs_whileTrue.py
@@ -0,0 +1,13 @@
+def while1():
+    x = 0
+    while 1:
+        x += 1
+        if x == 10:
+            break
+
+def whileTrue():
+    x = 0
+    while True:
+        x += 1
+        if x == 10:
+            break

9ac6a0601fcd4eafb4eca8c1ef1dabde61bc4b0d
jyshin9 <baekju092@gmail.com>
2023-07-13 13:21:21 +0900

    Commit

--- 1316.py
+++ 1316.py
@@ -1,2 +1,7 @@
 n = int(input())
 lst = list(map(int, input().split('\n')))
+
+#lst['lst[0]', 'lst[1]', 'lst[2]',...]
+for i in range(n-1): #lst 원소 인덱스
+    for j in range(len(str[i])): #lst i번째 원소의 j번째 글자
+        

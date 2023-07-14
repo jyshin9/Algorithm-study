9482f65e973bb70dfdb42c248955e82e83fe729b
jyshin9 <baekju092@gmail.com>
2023-07-14 17:45:40 +0900

    Commit

---
+++ 2525.py
@@ -0,0 +1,18 @@
+h,m = map(int, input().split())
+c = int(input())
+
+mm = m+c
+
+if(mm<60):
+    print(h,mm)
+elif(mm==60):
+    if(h==23):
+        print(0,0)
+    else:
+        print(h+1,0)
+else: #mm>60
+    if(h==23):
+        print(mm//60,mm%60)
+    else:
+        print(h+mm//60,mm%60)
+    

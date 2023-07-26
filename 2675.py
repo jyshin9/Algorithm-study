1e7eebfab9c29adf1e2f078d594e9b0a2945d615
jyshin9 <baekju092@gmail.com>
2023-07-26 09:24:14 +0900

    Commit

--- 2675.py
+++ 2675.py
@@ -1,9 +1,19 @@
 n = int(input())
+b = []
 
-num, a = [input().split() for _ in range(n)]
-num = int(num)
+for _ in range(n):
+    a = (list(input()))
+
+    c = int(a[0])
+    strr = ''
+
+    for j in range(2, len(a)):
+        strr += c*a[j]
+     
+    b.append(strr)
+
+    strr = '' #strr 초기화
+    a.clear() #a 초기화
 
 for i in range(n):
-    for j in range(len(a)-1):
-        print(a[j], end ="")*num
-    
+    print(b[i])

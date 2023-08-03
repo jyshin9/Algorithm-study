148d0e6464d8559a1fad49a56685903652cb2c9d
jyshin9 <baekju092@gmail.com>
2023-08-03 15:47:43 +0900

    Commit

--- 1009.py
+++ 1009.py
@@ -1,13 +1,15 @@
 import sys
+input = sys.stdin.readline
+end = [[10],[1],[2,4,8,6],[3,9,7,1],[4,6],[5],[6],[7,9,3,1],[8,4,2,6],[9,1]]
 
-n = int(sys.stdin.readline())
-result = []
+def whatisend(a,b):
+    Aend = a % 10 
+    Bend = b % len(end[Aend])
+    if Aend == 10:
+        return 0
+    else:
+        return end[Aend][Bend-1]
 
-for _ in range(n):
-    c = 0
-    a, b = map(int, sys.stdin.readline().split())
-    c = (a**b) % 10
-    result.append(c)
-
-for i in result:
-    print(i)
+for i in range(int(input())):
+    a, b = map(int, input().split())
+    print(whatisend(a,b))
---
+++ 1966.py
@@ -0,0 +1,27 @@
+from collections import deque #queue 함수
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    tqueue = deque(map(int, input().split())) #큐 선언 후 집어넣을 값 입력받기
+    init = deque(x for x in range(len(tqueue)))
+    target = tqueue[b]
+    th = 0 #몇번째로 출력되는지
+    
+    while True:
+        if tqueue[0] == max(tqueue): #i가 젤 크다면 => 출력해야함
+            if tqueue[0] == target and init[0] == b: #몇번째로 인쇄되는지 궁금한 친구라면
+                th += 1
+                print(th)
+                tqueue.clear()
+                break
+            else: 
+                tqueue.popleft()
+                init.popleft()
+                th += 1
+        else: 
+            tqueue.append(tqueue[0])
+            tqueue.popleft()
+            init.append(init[0])
+            init.popleft()

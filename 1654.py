8bc483b3f25e35cd2325e595d5ed2865765e18dd
jyshin9 <baekju092@gmail.com>
2023-08-10 16:02:36 +0900

    Commit

--- 1654.py
+++ 1654.py
@@ -1,13 +1,28 @@
+#이분탐색
+# def binary(arr, target, start, end):
+#     if end > start:
+#         return False
+    
+#     mid_index = (start+end)//2 #중앙값
+#     mid = arr[mid_index]
+    
+#     if mid == target:
+#         return True
+#     elif mid > target:
+#         return binary(arr, target, start, mid-1)
+#     elif mid < target:
+#         return binary(arr, target, mid+1, end)
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
-line, amount = [], 0 
+amount = 0
 
-for i in range(n):
-    line.append(int(input()))
+line = [int(input()) for _ in range(n)]
 
-#
-for i in line:
-    for j in range(i):
-        while i >= 0:
+for i in line: #오영식이 가지고 있는 랜선의 길이
+    for j in range(i): 
+        while i >= 0: #랜선이 사라지기 전까지
             i -= j
             amount += 1
                         
